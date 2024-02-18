1f85d000284a2cf9c12f165793b8b6a4115a4268
manisha-2703 <mandlipallimanisha123@gmail.com>
2024-02-18 20:10:51 +0530

    port changed

--- main.js
+++ main.js
@@ -65,7 +65,7 @@ sequelize.sync()
         console.error('Error syncing database:', error);
     });
 
-    const PORT = process.env.PORT || 4000;
+    const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
